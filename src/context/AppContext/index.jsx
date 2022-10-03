770e96c1b8f693e09fe36f98e4d2cdf094392a1a
Yan <yangabriel2012@gmail.com>
2022-10-03 08:38:14 -0300

    implementação das actions e do payload

--- src/context/AppContext/index.jsx
+++ src/context/AppContext/index.jsx
@@ -1,10 +1,14 @@
 import { createContext, useReducer, useState } from "react";
 import { globalState } from "./data";
+import { actions } from "./actions";
 import { reducer } from "./reducer";
 export const GlobalContext = createContext();
 
 export const AppCountext = (props) => {
   const [state, dispatch] = useReducer(reducer, globalState)
+  const changeTitle = (payload) => {
+    dispatch({ type: actions.CHANGE_TITLE, payload})
+  }
   console.log(state)
-  return <GlobalContext.Provider value={{state, dispatch}}>{props.children}</GlobalContext.Provider>
+  return <GlobalContext.Provider value={{state, changeTitle}}>{props.children}</GlobalContext.Provider>
 }
