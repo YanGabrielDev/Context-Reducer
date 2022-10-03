1f13f0f1da36f4f71a52f484e846c2905cd2e2cd
Yan <yangabriel2012@gmail.com>
2022-10-03 08:37:34 -0300

    implementação do use ref

--- src/components/H1/index.jsx
+++ src/components/H1/index.jsx
@@ -1,8 +1,12 @@
-import { useContext } from "react"
+import { useContext, useRef } from "react"
 import { GlobalContext } from "../../context/AppContext";
 export const H1 = () => {
+  const inputRef = useRef()
   const theContext = useContext(GlobalContext);
   return (
-    <h1>{theContext.state.title}</h1>
+    <>
+    <h1 onClick={() => console.log(theContext.changeTitle(inputRef.current.value))}>{theContext.state.title}</h1>
+    <input type="text" ref={inputRef}/>
+    </>
   );
 };
