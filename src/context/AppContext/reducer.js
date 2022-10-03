2961413221f5a558c60f00a785267389b6c6e11d
Yan <yangabriel2012@gmail.com>
2022-10-03 08:38:33 -0300

    modificações no reducer

--- src/context/AppContext/reducer.js
+++ src/context/AppContext/reducer.js
@@ -1,3 +1,10 @@
+import { actions } from "./actions";
 export const reducer = (state, action) => {
+  switch (action.type) {
+    case actions.CHANGE_TITLE: {
+      console.log("muda title");
+      return { ...state, title: action.payload };
+    }
+  }
   return { ...state };
 };
